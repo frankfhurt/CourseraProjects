03f2a016ecbb398f3a7e1192b425a01a5f160cbf
Franklyn <frankfhurt@gmail.com>
2017-01-12 22:30:40 -0200

    Commit inicial projeto da tarefa do modulo Principios de Desenvolvimento Agil de SW. Tarefa Livros a Jato com Gamificação.

---
+++ tarefa-livros-a-jato-gamificacao/src/main/java/br/com/livrosajato/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package br.com.livrosajato.controller;
+
+import javax.annotation.Resource;
+
+import br.com.caelum.vraptor.Get;
+
+@Resource
+public class UsuarioController {
+
+	@Get("/login")
+	public void loginForm() {
+		
+	}
+	
+}
